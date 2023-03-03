aa8134e74930a57cdad3829021deba2f836e8dc5
BranHill21 <bhill20@austincollege.edu>
2023-03-02 18:33:06 -0600

    Hello Twice

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		Greeter g = new Greeter();
-		
+		//Making code say hello twice just for a slight difference
+		g.greet();
 		g.greet();
 
 	}
